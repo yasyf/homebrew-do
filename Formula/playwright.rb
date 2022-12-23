5a7d0c09a7cbafa614a1dafc67f2d1657e6a9640
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-23 12:32:18 +0000

    playwright: add 1.29.1 bottle.

--- Formula/playwright.rb
+++ Formula/playwright.rb
@@ -7,6 +7,13 @@ class Playwright < Formula
   sha256 "db072864b9d7722c617c74410f56e57fbdaeb010dd8023e84a3e4a75cf7d07db"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/yasyf/homebrew-do/releases/download/playwright-1.29.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, monterey:     "4b9b8d41fbd2b9b55f8f20b521f26e1926ab4a732decb0b7dcafe5f4a2f2562c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c0f79622e46f3b0309bd166b0e7ceb0a86a2b9288aaab302ba93132d4ae8fa84"
+  end
+
   depends_on "node"
 
   def install
