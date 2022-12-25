d6e0c4fafe25c8d4f2fd46ab49817bdf863f8910
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-25 07:48:10 -0500

    do: rebuild
    * Add source for playwright
    * style
    * Gate playwright installation
    
    Closes #4.
    
    Signed-off-by: Yasyf Mohamedali <yasyfm@gmail.com>

--- Formula/do.rb
+++ Formula/do.rb
@@ -10,8 +10,8 @@ class Do < Formula
   bottle do
     root_url "https://github.com/yasyf/homebrew-do/releases/download/do-0.1.15"
     sha256 cellar: :any_skip_relocation, arm64_ventura: "00a8a9a751624b5b7c48996b0cc6c41d12abae3d001303c11c0a212df2b76d18"
-    sha256 cellar: :any_skip_relocation, monterey:     "bf3804347624b6bf2c4dddae30d2b009de6adbd32eacf9991e790c29599f23ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "321d736af5e8abbc4152a22f3dcadd63d0e937eb8c162d1d6d29856fcd146a63"
+    sha256 cellar: :any_skip_relocation, monterey:      "bf3804347624b6bf2c4dddae30d2b009de6adbd32eacf9991e790c29599f23ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "321d736af5e8abbc4152a22f3dcadd63d0e937eb8c162d1d6d29856fcd146a63"
   end
 
   option "with-playwright"
@@ -21,6 +21,11 @@ class Do < Formula
   if build.with?("playwright")
     depends_on "node"
     depends_on "yasyf/do/playwright"
+
+    resource "playwright" do
+      url "https://github.com/microsoft/playwright-python/archive/refs/tags/v1.29.0.tar.gz"
+      sha256 "f4c5d82d9b7c35435fa840dee15f1c4528bfaf0bdffb796a65d6071747c99f7b"
+    end
   end
 
   resource "certifi" do
@@ -168,7 +173,7 @@ class Do < Formula
     system "python3.10", "-m", "venv", "--upgrade", "--upgrade-deps", root
     system root/"bin"/"pip", "install", "-U", "wheel", "packaging"
 
-    venv.pip_install resources.filter { |r| r.url.include? "pythonhosted" }
+    venv.pip_install resources
     venv.pip_install_and_link buildpath
   end
 
